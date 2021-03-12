b44b28406148b61b5106f507c9de9d7988f9aa99
Charlie Poliwoda <cxp6583@rit.edu>
2021-03-12 09:51:03 -0500

    DC Motor Works - Part 1

--- code/main.c
+++ code/main.c
@@ -20,53 +20,51 @@ int main(void) {
 	// Print welcome over serial
 	UART0_Put("Running... \n\r");
 	
-	/* Part 1 - UNCOMMENT THIS
+	//PART 1
 	// Generate 20% duty cycle at 10kHz
-	// INSERT CODE HERE
+	//FTM0_set_duty_cycle(20,10000,1); //20% duty cycle, 10kHz, forward direction
+	//for(;;) ;  //then loop forever
 	
-	for(;;) ;  //then loop forever
-	*/
 	
-	/* Part 2 - UNCOMMENT THIS
+	//PART 2
 	for(;;)  //loop forever
 	{
 		uint16_t dc = 0;
 		uint16_t freq = 10000; // Frequency = 10 kHz 
 		uint16_t dir = 0;
 		char c = 48;
-		int i=0;
+		unsigned int i=0;
 		
 		// 0 to 100% duty cycle in forward direction
-		for (i=0; i<100; i++) {
-		    // INSERT CODE HERE
+		for (i=0; i<=100; i++) {
+		    FTM0_set_duty_cycle(i,freq,~dir);
 			
 			delay(10);
 		}
 		
 		// 100% down to 0% duty cycle in the forward direction
-		for (i=100; i>=0; i--) {
-		    // INSERT CODE HERE
+		for (i=100; i>0; i--) {
+		    FTM0_set_duty_cycle(i,freq,~dir);
 			
 			delay(10);
 		}
+		
 		
 		// 0 to 100% duty cycle in reverse direction
-		for (i=0; i<100; i++) {
-		    // INSERT CODE HERE
+		for (i=0; i<=100; i++) {
+		    FTM0_set_duty_cycle(i,freq,dir);
 			
 			delay(10);
 		}
 		
+		
 		// 100% down to 0% duty cycle in the reverse direction
-		for (i=100; i>=0; i--) {
-		    // INSERT CODE HERE
+		for (i=100; i>0; i--) {
+		    FTM0_set_duty_cycle(i,freq,dir);
 			
 			delay(10);
 		}
-
 	}
-	*/
-	return 0;
 }
 
 
@@ -81,3 +79,34 @@ void delay(int del){
 		// Do nothing
 	}
 }
+
+/*
+// Enable clocks on Port D
+
+// Configure the Signal Multiplexer for the Port D GPIO Pins
+
+// Configure the GPIO Pins for Output
+
+int forward = 1;
+int phase = 0;
+
+while(true){
+	// turn off all coils, Set GPIO pins to 0
+
+	// set one pin high at a time
+	if(forward){
+		if(phase == 0){//turn on coil A; phase++;} //A, 1a
+		else if(phase == 1){//turn on coil B; phase++;} //B, 2b
+		else if(phase == 2){//turn on coil C; phase++;} //C, 1b
+		else{//turn on coil D; phase = 0;} //D, 2b
+	}
+	else{ //reverse
+	if(phase == 0){//turn on coil D; phase++;} //D, 2b
+		else if(phase == 1){//turn on coil C; phase++;} //C, 1b
+		else if(phase == 2){//turn on coil B; phase++;} //B, 2a
+		else{//turn on coil A; phase = 0;} //A, 1a
+	}
+	//NOTE: you need to write your own delay function??
+	delay(10);
+}
+*/
